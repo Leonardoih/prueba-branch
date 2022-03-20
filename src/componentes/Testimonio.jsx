d96a22a5985c9b921f43ed3204feaf85b4e434b2
leonardoih <leonardoih090217@gamil.com>
2022-03-20 15:13:46 -0500

    terminado

--- src/componentes/Testimonio.jsx
+++ src/componentes/Testimonio.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return (
     <div className='contenedor-testimonio'>
       <img
         className='imagen-testimonio'
-        src={require('../imagenes/testimonio-emma.png')}
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
         alt='Imagen de emma'
       />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
-        <p className='cargo-testimonio'>Ingenieria de Software en Spotify</p>
+        <p className='nombre-testimonio'><strong>{props.nombre}</strong> en <strong>{props.pais}</strong></p>
+        <p className='cargo-testimonio'>{props.cargo} en <strong>{props.empresa}</strong></p>
         <p className='texto-testimonio'>
-          "Siempre he tenido problemas para aprender JavaScript. He tomado muchos
-          cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar
-          JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp
-          me dio las habilidades y la confianza que necesitaba para conseguir el
-          trabajo de mis sueños como ingeniero de software en Spotify."
+          "{props.testimonio}"
         </p>
       </div>
     </div>
