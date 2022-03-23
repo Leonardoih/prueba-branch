8c2976d9fa57afcb040460a5fb12bc32f739bfbb
leonardoih <leonardoih090217@gamil.com>
2022-03-22 23:08:14 -0500

    nuevo comentario

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         testimonio='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
       />
       <Testimonio
-        nombre='Sarah Chima'
+        nombre='Sarah Chima nuevo'
         alt='foto de Sarah'        
         pais='Nigeria'
         imagen='sarah'
